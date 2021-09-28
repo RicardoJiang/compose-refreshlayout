78b61ec4cdfa72338ee74915c8a7e3cc4ffa416a
shenzhen2017 <2868405029@qq.com>
2021-09-28 09:26:55 +0800

    ADD: headerview箭头动画

--- app/src/main/java/com/zj/composerefreshlayout/ClassicRefreshHeader.kt
+++ app/src/main/java/com/zj/composerefreshlayout/ClassicRefreshHeader.kt
@@ -1,17 +1,22 @@
 package com.zj.composerefreshlayout
 
-import androidx.compose.foundation.layout.Box
-import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.foundation.layout.height
+import androidx.compose.animation.core.Animatable
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.*
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.runtime.remember
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clipToBounds
+import androidx.compose.ui.draw.rotate
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.unit.dp
+import com.google.accompanist.drawablepainter.rememberDrawablePainter
+import com.zj.composerefreshlayout.header.ArrowDrawable
 
 @Composable
 fun ClassicRefreshHeader(state: SwipeRefreshState) {
@@ -20,23 +25,45 @@ fun ClassicRefreshHeader(state: SwipeRefreshState) {
             "下拉刷新"
         }
         RefreshHeaderState.Refreshing -> {
-            "正在刷新"
+            "正在刷新..."
         }
         else -> {
             "释放刷新"
         }
     }
-    Box(modifier = Modifier
-        .fillMaxWidth()
-        .height(80.dp), contentAlignment = Alignment.Center) {
-        Text(
-            text = text,
-            color = Color.Black,
-            textAlign = TextAlign.Center,
-            style = MaterialTheme.typography.h5,
-            modifier = Modifier
-                .fillMaxWidth()
-                .clipToBounds()
-        )
+    val angle = remember {
+        Animatable(0f)
+    }
+    LaunchedEffect(key1 = state.headerState) {
+        if (state.headerState == RefreshHeaderState.ReleaseToRefresh) {
+            angle.animateTo(180f)
+        } else {
+            angle.animateTo(0f)
+        }
+    }
+    Box(
+        modifier = Modifier
+            .fillMaxWidth()
+            .height(80.dp), contentAlignment = Alignment.Center
+    ) {
+        Row(verticalAlignment = Alignment.CenterVertically) {
+            Image(
+                painter = rememberDrawablePainter(drawable = ArrowDrawable()),
+                contentDescription = "",
+                modifier = Modifier
+                    .size(20.dp)
+                    .rotate(angle.value)
+            )
+            Text(
+                text = text,
+                color = Color.Black,
+                textAlign = TextAlign.Center,
+                style = MaterialTheme.typography.body1,
+                modifier = Modifier
+                    .wrapContentSize()
+                    .clipToBounds()
+                    .padding(16.dp, 0.dp)
+            )
+        }
     }
 }
